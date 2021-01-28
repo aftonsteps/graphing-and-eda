c3e4ad17f91c65be5b3ad5cff9a7c823d9418364
aftonsteps <aftoncoombs@gmail.com>
2021-01-27 23:08:26 -0800

    Added Bernie graphs
    - Did a little EDA of Bernie Google Trends data
    - Made graphs with geom_bernie()

--- vignettes/bernie/bernie_source.R
+++ vignettes/bernie/bernie_source.R
@@ -1,5 +1,6 @@
 ## Gets Bernie and mittens data
 
+## Bernie data most recently
 bernie_7_days <-
   gtrendsR::gtrends(keyword = "bernie", time = "now 7-d")
 bernie_mittens_7_days <-
@@ -8,3 +9,10 @@ mittens_7_days <-
   gtrendsR::gtrends(keyword = "mittens", time = "now 7-d")
 save(bernie_7_days, bernie_mittens_7_days, mittens_7_days,
      file = "vignettes/bernie/bernie_data.Rdata")
+
+
+## Bernie data historically
+## Retrieved at approx. 2021-01-27 21:55:44 PST
+bernie_over_time <-
+  gtrendsR::gtrends(keyword = "bernie", time = "all")
+save(bernie_over_time, file = "vignettes/bernie/long_term_bernie_data.Rdata")
